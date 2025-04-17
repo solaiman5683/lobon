6b3dabb1f0877f012b89d17e5737755a62f23f86
Muhammad Sulaiman Hosain <solaiman.sh6@gmail.com>
2025-04-17 11:39:45 +0600

    refactor: update layout and styling of platforms component for improved responsiveness

--- app/(root)/platform/_components/platforms.tsx
+++ app/(root)/platform/_components/platforms.tsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 export default function Platforms()
 {
     return (
-        <div className="grid lg:grid-cols-5 grid-cols-2 justify-center items-center py-12 gap-4">
-            <Link href='/'>
-                <Image src="/images/playstore.png" alt="Play Store" width={300} height={100} className="w-[200px] h-auto" priority />
+        <div className="flex flex-wrap justify-center items-center py-12 lg:gap-5 gap-3">
+            <Link href='/' className="md:w-fit w-[45%]">
+                <Image src="/images/playstore.png" alt="Play Store" width={300} height={100} className="md:h-[45px] w-full h-auto md:w-auto" priority />
             </Link>
-            <Link href='/'>
-                <Image src="/images/appstore.png" alt="App Store" width={300} height={100} className="w-[200px] h-auto" priority />
+            <Link href='/' className="md:w-fit w-[45%]">
+                <Image src="/images/appstore.png" alt="App Store" width={300} height={100} className="md:h-[45px] w-full h-auto md:w-auto" priority />
             </Link>
-            <Link href='/'>
-                <Image src="/images/windstore.png" alt="Windows Store" width={300} height={100} className="w-[200px] h-auto" priority />
+            <Link href='/' className="md:w-fit w-[45%]">
+                <Image src="/images/windstore.png" alt="Windows Store" width={300} height={100} className="md:h-[45px] w-full h-auto md:w-auto" priority />
             </Link>
-            <Link href='/'>
-                <Image src="/images/macos.png" alt="Mac OS" width={300} height={100} className="w-[200px] h-auto" priority />
+            <Link href='/' className="md:w-fit w-[45%]">
+                <Image src="/images/macos.png" alt="Mac OS" width={300} height={100} className="md:h-[45px] w-full h-auto md:w-auto" priority />
             </Link>
-            <Link href='/'>
-                <Image src="/images/linux.png" alt="Linux" width={300} height={100} className="w-[200px] h-auto" priority />
+            <Link href='/' className="md:w-fit w-[45%]">
+                <Image src="/images/linux.png" alt="Linux" width={300} height={100} className="md:h-[45px] w-full h-auto md:w-auto" priority />
             </Link>
         </div>
     )
